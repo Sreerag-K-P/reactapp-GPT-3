e535f950a499717a5cf5acdff89623ad45483826
Sreerag-K-P <sreeragkjnivas4324602@gmail.com>
2023-04-30 22:46:43 +0530

    day1+day2+day3+day4+day5-compleate

---
+++ src/components/article/Article.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './article.css';
+
+
+const Article = ({imgUrl,date,text}) => {
+  return (
+    <div className='gpt3__blog-container_article'>
+      <div className="gpt3__blog-container_article-image">
+        <img src={imgUrl} alt="" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+            <div>
+             <p>{date}</p>
+             <h3 className='gradient__text'>{text}</h3>
+            </div>
+            <p>Reade Full Article</p>
+      </div>
+    </div>
+  )
+};
+
+export default Article
---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './brand.css';
+
+import { google,slack,atlassian,dropbox,shopyfy } from './import';
+const Brand = () => {
+  return (
+    <div className='gpt3__brand section__padding'>
+      <div>
+        <img src={google} alt="google" />
+      </div>
+      <div>
+        <img src={slack} alt="slack" />
+      </div>
+      <div>
+        <img src={atlassian} alt="atlassian" />
+      </div>
+      <div>
+        <img src={dropbox} alt=" dropbox" />
+      </div>
+      <div>
+        <img src={shopyfy} alt="shopyfy" />
+      </div>
+
+      
+    </div>
+  )
+}
+
+export default Brand
